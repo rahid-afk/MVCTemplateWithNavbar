8cbe195473c6b81ea436a4a4e4a79fb6a713b3c6
rahid <rahidmiah4@gmail.com>
2023-10-24 19:00:50 +0100

    added Calculator files: exercise 5.2 done

--- MVCtemplate-22-23-with-top-navbar/Models/Calculator.php
+++ MVCtemplate-22-23-with-top-navbar/Models/Calculator.php
@@ -2,9 +2,28 @@
 
 class Calculator
 {
-
-    public function __construct()
+/* Class to build a calculator */
+    var $firstNumber = 0, $secondNumber = 0, $operator;
+    public function __construct($firstNumber, $secondNumber, $operator)
     {
+        $this->firstNumber = $firstNumber;
+        $this->secondNumber = $secondNumber;
+        $this->operator = $operator;
+    }
 
+    public function calculate(){
+        if ($this->operator == "add"){
+            $result = $this->firstNumber + $this->secondNumber;
+        }
+        if ($this->operator == "subtract"){
+            $result = $this->firstNumber - $this->secondNumber;
+        }
+        if ($this->operator == "multiply"){
+            $result = $this->firstNumber * $this->secondNumber;
+        }
+        if ($this->operator == "divide"){
+            $result = $this->firstNumber / $this->secondNumber;
+        }
+        return $result;
     }
 }
--- MVCtemplate-22-23-with-top-navbar/calculator.php
+++ MVCtemplate-22-23-with-top-navbar/calculator.php
@@ -3,6 +3,9 @@ $view = new stdClass();
 $view->pageTitle = 'Calculator';
 require_once('Models/Calculator.php');
 if (isset($_POST['submit'])){
-
+    $calculator = new Calculator($_POST['firstNumber'], $_POST['secondNumber'], $_POST['operator']);
+    $value = $calculator->calculate();
+    $view->result = 'Calculating ' . $_POST['firstNumber'] . ' '
+        . $_POST['operator'] . ' ' . $_POST['secondNumber'] . ' is ' . $value . '.';
 }
 require_once('Views/calculator.phtml');
