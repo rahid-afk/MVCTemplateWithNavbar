09a527367c305d50b36b02edec7ad1d914bef377
rahid-afk <rahidmiah4@gmail.com>
2023-10-25 22:02:45 +0100

    updated Calculator files: exercise 5.3 done

--- MVCtemplate-22-23-with-top-navbar/Models/Calculator.php
+++ MVCtemplate-22-23-with-top-navbar/Models/Calculator.php
@@ -3,27 +3,50 @@
 class Calculator
 {
 /* Class to build a calculator */
-    var $firstNumber = 0, $secondNumber = 0, $operator;
-    public function __construct($firstNumber, $secondNumber, $operator)
-    {
-        $this->firstNumber = $firstNumber;
-        $this->secondNumber = $secondNumber;
-        $this->operator = $operator;
+//    var $firstNumber = 0, $secondNumber = 0, $operator;
+//    public function __construct($firstNumber, $secondNumber, $operator)
+//    {
+//        $this->firstNumber = $firstNumber;
+//        $this->secondNumber = $secondNumber;
+//        $this->operator = $operator;
+//    }
+//
+//    public function calculate(){
+//        if ($this->operator == "add"){
+//            $result = $this->firstNumber + $this->secondNumber;
+//        }
+//        if ($this->operator == "subtract"){
+//            $result = $this->firstNumber - $this->secondNumber;
+//        }
+//        if ($this->operator == "multiply"){
+//            $result = $this->firstNumber * $this->secondNumber;
+//        }
+//        if ($this->operator == "divide"){
+//            $result = $this->firstNumber / $this->secondNumber;
+//        }
+//        return $result;
+//    }
+
+    var $expression;
+    public function __construct($expression){
+        $this->expression = $expression;
     }
 
     public function calculate(){
-        if ($this->operator == "add"){
-            $result = $this->firstNumber + $this->secondNumber;
+        $nodes = explode(" ", $this->expression);
+        if ( $nodes[1] == "+"){
+            $result = $nodes[0] + $nodes[2];
         }
-        if ($this->operator == "subtract"){
-            $result = $this->firstNumber - $this->secondNumber;
+        if ( $nodes[1] == "-"){
+            $result = $nodes[0] - $nodes[2];
         }
-        if ($this->operator == "multiply"){
-            $result = $this->firstNumber * $this->secondNumber;
+        if ( $nodes[1] == "*"){
+            $result = $nodes[0] * $nodes[2];
         }
-        if ($this->operator == "divide"){
-            $result = $this->firstNumber / $this->secondNumber;
+        if ( $nodes[1] == "/"){
+            $result = $nodes[0] / $nodes[2];
         }
         return $result;
+
     }
 }
--- MVCtemplate-22-23-with-top-navbar/calculator.php
+++ MVCtemplate-22-23-with-top-navbar/calculator.php
@@ -3,9 +3,12 @@ $view = new stdClass();
 $view->pageTitle = 'Calculator';
 require_once('Models/Calculator.php');
 if (isset($_POST['submit'])){
-    $calculator = new Calculator($_POST['firstNumber'], $_POST['secondNumber'], $_POST['operator']);
+//    $calculator = new Calculator($_POST['firstNumber'], $_POST['secondNumber'], $_POST['operator']);
+    $calculator = new Calculator($_POST['expression']);
     $value = $calculator->calculate();
-    $view->result = 'Calculating ' . $_POST['firstNumber'] . ' '
-        . $_POST['operator'] . ' ' . $_POST['secondNumber'] . ' is ' . $value . '.';
+//    $view->result = 'Calculating ' . $_POST['firstNumber'] . ' '
+//        . $_POST['operator'] . ' ' . $_POST['secondNumber'] . ' is ' . $value . '.';
+
+    $view->result = "Calculating " . $_POST['expression'] . " " . " is " . $value . ".";
 }
 require_once('Views/calculator.phtml');
